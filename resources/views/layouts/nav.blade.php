b7b8684fc88a69d688015300964a6a84f85f8ae3
shaon-sarker <shaon180054+github@diit.info>
2021-08-19 11:57:24 +0600

    "custom logout set"

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -68,7 +68,13 @@
             <li><a href=""><i class="icon ion-ios-download-outline"></i> Downloads</a></li>
             <li><a href=""><i class="icon ion-ios-star-outline"></i> Favorites</a></li>
             <li><a href=""><i class="icon ion-ios-folder-outline"></i> Collections</a></li>
-            <li><a href=""><i class="icon ion-power"></i> Sign Out</a></li>
+            <li>
+                <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();"><i class="icon ion-power"></i> Sign Out</a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </li>
           </ul>
         </div><!-- dropdown-menu -->
       </div><!-- dropdown -->
